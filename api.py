589c8ee7eeed4bf73cc363756d02b210c131ffa9
Abdullah Basaran <abdullahbsrn8@gmail.com>
2020-05-05 17:05:44 +0300

    he77

---
+++ api.py
@@ -0,0 +1,18 @@
+import requests
+import pprint
+
+city = input('Enter your city:')
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=1d0bdcc3ac9ff7582a02adab86f59c6b'.format(city)
+
+res = requests.get(url)
+
+data = res.json()
+
+temp = data['main']['temp']
+wind_speed = data['wind']['speed']
+
+print('Temperature : ',temp)
+print('Wind Speed : ',wind_speed)
+
+bhh
