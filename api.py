8711c9f68ea35a308d04ea2a94a722297fe0aced
Abdullah Basaran <abdullahbsrn8@gmail.com>
2020-05-06 01:55:42 +0300

    ilce bilgisi ile veri cekildi

--- api.py
+++ api.py
@@ -1,17 +1,34 @@
 import requests
 import pprint
+import pandas as pd
+from unidecode import unidecode
 
-city = input('Enter your city:')
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=1d0bdcc3ac9ff7582a02adab86f59c6b'.format(city)
+excel_data_df = pd.read_excel('ilce_merkez_coord.xlsx') #excel den dataframe e donusturme
+
+for col in ['ILCEADI']:  
+    excel_data_df[col] = excel_data_df[col].apply(unidecode)  #turkce karakterler decode edildi
+
+print(excel_data_df)
+
+ilce = excel_data_df.loc[0][5]
+
+url = 'https://api.weatherstack.com/current?access_key=51d7bfab86b02431a6a6cc5d489ea60b&query='+ilce
 
 res = requests.get(url)
-
 data = res.json()
 
-temp = data['main']['temp']
+time = data['location']['localtime']
+
+
+print('Time : ',time)
+#print(data)
+
+
+'''temp = data['main']['temp']
 wind_speed = data['wind']['speed']
 
 print('Temperature : ',temp)
 print('Wind Speed : ',wind_speed)
 
+'''
